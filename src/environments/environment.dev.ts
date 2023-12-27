df48d949d563d94459a14143155fdea9a75221a1
Swapnil Opash <sra.opash@gmail.com>
2023-12-27 19:12:07 +0530

    initial commit

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,25 @@
+const loginUrl = 'https://www.veterinarian.tube/login';
+const logoutUrl = 'https://www.veterinarian.tube/logout';
+const frontendUrl = 'https://video.veterinarian.tube/';
+const backendUrl =  'https://api.veterinarian.tube/';
+
+// const frontendUrl = 'http://localhost:4200/';
+// const backendUrl = 'http://localhost:8080/';
+// const loginUrl = 'http://localhost:4200/login';
+const wasabiUrl = 'https://freedom-social.s3.us-east-1.wasabisys.com/'
+
+
+export const environment = {
+  production: false,
+  frontendUrl: frontendUrl,
+  backendUrl: backendUrl,
+  loginUrl: loginUrl,
+  apiUrl: `${backendUrl}api/v1/`,
+  domain: '.veterinarian.tube',
+  wasabiUrl: wasabiUrl,
+  socketUrl: `${backendUrl}`,
+  conferenceUrl: 'https://facetime.tube/',
+  logoutUrl: logoutUrl
+
+};
+
